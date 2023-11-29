2a08a3d040961cba781f0edfd812d7777eca4340
Pablo-ED1 <a_pablo.aguilera.salinas@iespablopicasso.es>
2023-11-29 10:10:38 +0100

    Primer commit

---
+++ src/Persona.java
@@ -0,0 +1,34 @@
+public class Persona {
+
+    String nombre;
+    String apellidos;
+    int edad;
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+    public void Cumpleaños() {
+        edad++;
+        System.out.println("Feliz cumpleaños! " + nombre + " ahora tiene " + edad + " años.");
+    }
+    public static void main(String[] args) {
+      Persona persona = new Persona("Pablo", "Aguilera", 18);
+
+      System.out.println("Antes del cumpleaños:");
+      System.out.println("Nombre: " + persona.nombre);
+      System.out.println("Apellidos: " + persona.apellidos);
+      System.out.println("Edad: " + persona.edad);
+
+      persona.Cumpleaños();
+
+      System.out.println("Después del cumpleaños:");
+      System.out.println("Nombre: " + persona.nombre);
+      System.out.println("Apellidos: " + persona.apellidos);
+      System.out.println("Edad: " + persona.edad);
+  }
+}    
+
+    
